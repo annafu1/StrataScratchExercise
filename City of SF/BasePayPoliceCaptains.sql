a64703e490934a5fab859208810e33373905676a
annafu1 <104175541+annafu1@users.noreply.github.com>
2023-02-06 17:47:14 -0500

    base pay found

---
+++ City of SF/BasePayPoliceCaptains.sql
@@ -0,0 +1,4 @@
+--Find the base pay for Police Captains.
+--Output the employee name along with the corresponding base pay.
+select employeename,basepay from sf_public_salaries
+where jobtitle like '%Captain%Police%';

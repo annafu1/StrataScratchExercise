bbab7bd661f6923dbfecdcc81452ed1fd27f9afc
annafu1 <104175541+annafu1@users.noreply.github.com>
2023-02-06 17:46:09 -0500

    found libraries from 2016 without any emails

---
+++ City of SF/LibrariesWithNoEmail.sql
@@ -0,0 +1,4 @@
+--Find libraries who haven't provided the email address in circulation year 2016 but their notice preference definition is set to email.
+--Output the library code.
+select distinct(home_library_code) from library_usage
+where circulation_active_year = 2016 and notice_preference_definition = 'email' and provided_email_address = 0;

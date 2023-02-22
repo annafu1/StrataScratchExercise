3eb0d8b0e8011c78d88b5577abf0212b11d2fd26
Anna <annahuang068@gmail.com>
2023-02-21 19:03:39 -0500

    added in alias

--- Amazon/HighestCostOrder.sql
+++ Amazon/HighestCostOrder.sql
@@ -1,8 +1,9 @@
 -- Find the customer with the highest daily total order cost between 2019-02-01 to 2019-05-01. If customer had more than one order on a certain day, sum the order costs on daily basis. 
 -- Output customer's first name, total cost of their items, and the date.
-select first_name,sum(total_order_cost) as total_cost, order_date from customers
-join orders on customers.id = orders.id
-where orders_date between '2019-02-01' and '2019-05-01'
-group by orders.order_date, customers.first_name
+select c.first_name, o.order_date, sum(o.total_order_cost) as total_cost
+from customers as c join orders as o 
+on c.id = o.cust_id
+where order_date between '2019-02-01' and '2019-05-01'
+group by o.order_date, c.first_name
 order by total_cost desc
 limit 1;
